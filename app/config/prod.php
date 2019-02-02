8e054551ed3818bcc2e695c416482f25f33d101b
Olga LItskevich <olha.litskevich@gmail.com>
2019-02-02 15:13:47 +0300

    create prod.php

---
+++ app/config/prod.php
@@ -0,0 +1,9 @@
+<?php
+$app['db.options'] = array(
+    "driver"     => "pdo_pgsql",
+    "host"       => "localhost",
+    "dbname"     =>"postgres",
+    "user"       => "postgres",
+    "port"       =>"5432",
+    "password"   => "",
+);
