af0a926e7169d963a49e1f98358744a4e3702423
Olga LItskevich <olha.litskevich@gmail.com>
2019-02-03 22:44:30 +0300

    +

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 // /web/index.php
 $app = require_once __DIR__.'/../app/app.php';
 require __DIR__ . '/../app/config/prod.php';
+
 //Services
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../resources/views'
@@ -13,15 +14,34 @@ $app->register(new \Silex\Provider\DoctrineServiceProvider(), array(
 
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 
-$app->register(new MonologServiceProvider, array(
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__ . '/logs/app.log',
 ));
 
+/*$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'secure' => array(
+            'anonymous' => true,
+            'pattern' => '^.*$',
+            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+            'logout' => array('logout_path' => '/logout'),
+            'users' => function () use ($app) {
+                return new MyApp\User\UserProvider($app['db']);
+            },
+        )
+    )
+));*/
+//Определение правил доступа
+//$app['security.access_rules'] = array(
+//    array('^/admin', 'ROLE_ADMIN'),
+//    array('^.*$', 'ROLE_USER'),
+//);
 $app->mount("/users", new MyApp\Controller\Providers\Users());
 $app->mount("/books", new MyApp\Controller\Providers\Books());
 $app->mount("/orders", new MyApp\Controller\Providers\Orders());
 $app->mount("/authors", new MyApp\Controller\Providers\Authors());
 
 
+$app['route_class'] = 'MyRoute';
 
 $app->run();
